4da6235b31b4d40b2059c0d443a5287b65b2f49d
Clayton <wmjh0925@naver.com>
2023-10-12 00:34:06 +0900

    :sparkles: HomeScreen에서 유튜브 화면이 출력되도록 구현

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cf_tube/component/custom_youtube_player.dart';
+import 'package:cf_tube/model/video_model.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,7 +8,13 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      backgroundColor: Colors.black,
+      body: CustomYoutubePlayer(
+        videoModel: VideoModel(
+          id: 'bTWbQSHrEL4',                  // 샘플 동영상 ID
+          title: '매년 달라지는 한정판 추석 선물과 함께한 기분 좋은 ASMR, 들어보실래요? │ 게임회사 문화 │ 트리노드',  // 샘플 제목
+        ),
+      ),
     );
   }
 }

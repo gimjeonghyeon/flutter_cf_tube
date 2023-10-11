da7801b07f5094028105d3197209f242b1a49395
Clayton <wmjh0925@naver.com>
2023-10-12 01:23:34 +0900

    :bug: 유튜브 API 및 채널 아이디를 유효한 값으로 변경

--- lib/const/api.dart
+++ lib/const/api.dart
@@ -1,4 +1,4 @@
 const API_KEY = 'AIzaSyDROUIwZ8TqFqsMoj5qKQKZl1VnOipKFlw';
 // 유튜브 Data API V3 URL
-const YOUTUBE_API_BASE_URL = 'http://youtube.googleapis.com/youtube/v3/search';
-const CF_CHANNEL_ID = '@treenod_story'; // 채널 ID
+const YOUTUBE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+const CF_CHANNEL_ID = 'UCyRIhtMNZfY5jxAGaPi0e7w'; // 채널 ID

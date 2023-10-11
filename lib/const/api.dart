eaa551083a3f8236b759421969c29c1141f57ac0
Clayton <wmjh0925@naver.com>
2023-10-12 00:57:55 +0900

    :sparkles: 유튜브 API 통신을 위한 repository 구현

---
+++ lib/const/api.dart
@@ -0,0 +1,4 @@
+const API_KEY = 'AIzaSyDROUIwZ8TqFqsMoj5qKQKZl1VnOipKFlw';
+// 유튜브 Data API V3 URL
+const YOUTUBE_API_BASE_URL = 'http://youtube.googleapis.com/youtube/v3/search';
+const CF_CHANNEL_ID = '@treenod_story'; // 채널 ID
